55ab7e6b77a39b9ddda23fdae97a150a7fe49c63
izu <izu>
2024-10-25 17:59:13 +0200

    add TMSS skip

--- inc/nihil.h
+++ inc/nihil.h
@@ -11,5 +11,16 @@ extern void c_vdp_reset ( void ) __asm__("vdp_reset");
 extern void c_vdp_clearmem ( void ) __asm__("vdp_clearmem");
 extern void c_vdp_wait_vblank ( void ) __asm__("vdp_wait_vblank");
 
+void c_vdp_write_cram_cpu ( int palette_number, void* data_pointer ) 
+{
+    __asm__ volatile (
+        "move.b	%0, %%d0\n\t"
+        "move.l %1, %%a0\n\t"
+        "jsr vdp_write_cram_cpu"
+        :
+        : "r" ( palette_number ), "r" ( data_pointer )
+        : "d0", "a0"
+    );
+}
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "nihil.h"
 
+extern const char bruji_pal[];
 int main ( void )
 {
 	c_vdp_reset ();
 	c_vdp_clearmem ();
+	c_vdp_write_cram_cpu ( 0, ( void* ) bruji_pal );
 	
 	while ( 1 )
 	{

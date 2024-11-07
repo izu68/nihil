373592858e09e3b2b79a9d41588e31ecbf278915
izu <izu@izus-MacBook-Air.local>
2024-11-07 14:34:09 +0100

    add WRAM clear and prevent TMSS

--- src/main.c
+++ src/main.c
@@ -1,20 +1,15 @@
 int main ( void )
 {
+	// Reset vdp and clear VRAM, CRAM and VSRAM
 	__asm__ volatile
 	(
 		"bsr	vdp_reset\n"
 		"bsr	vdp_clearmem"
 	);
-	__asm__ volatile
-	(
-		"move.b	#0x00, 0xA130F0\n"
-		"move.b #0x01, 0xA13070\n"
-		"move.b #0x03, 0xA13073\n"
-		"move.b #0xFF, 0xA130F0"
-	);
 	
 	while ( 1 )
 	{
+		// Wait for vblank
 		__asm__ volatile
 		(
 		"bsr	vdp_wait_vblank"

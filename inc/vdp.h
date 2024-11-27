8246833ba7ede7184010eefa70b3168cac9c5794
izu <izu>
2024-11-27 21:41:59 +0100

    fixed some inconsistencies between C forwards

--- inc/vdp.h
+++ inc/vdp.h
@@ -7,21 +7,21 @@ extern void vdp_reset (void);
 extern void vdp_clear_mem (void);
 extern void vdp_wait_vblank (void);
 
-void c_vdp_write_vram_cpu_w (u16 vram_addr, u16 words_num, void* data_ptr)
+void c_vdp_write_vram_cpu (u16 size_words, u16 dest, void* data_ptr)
 {
 	__asm__ volatile
 	(
 		"move.w %0, %%d0\n\t"
 		"move.w %1, %%d1\n\t"
 		"move.l %2, %%a0\n\t"
-		"bsr vdp_write_vram_cpu_w"
+		"bsr vdp_write_vram_cpu"
 		:
-		: "r" ( vram_addr ), "r" ( words_num ), "m" ( data_ptr )
+		: "r" ( dest ), "r" ( size_words ), "m" ( data_ptr )
 		: "d0", "d1", "a0"
 	);
 }
 
-void c_vdp_write_vram_cpu_l (u16 vram_addr, u16 longs_num, void* data_ptr)
+void c_vdp_write_vram_cpu_l (u16 size_longs, u16 dest, void* data_ptr)
 {
 	__asm__ volatile
 	(
@@ -30,7 +30,7 @@ void c_vdp_write_vram_cpu_l (u16 vram_addr, u16 longs_num, void* data_ptr)
 		"move.l %2, %%a0\n\t"
 		"bsr vdp_write_vram_cpu_l"
 		:
-		: "r" ( vram_addr ), "r" ( longs_num ), "m" ( data_ptr )
+		: "r" ( dest ), "r" ( size_longs ), "m" ( data_ptr )
 		: "d0", "d1", "a0"
 	);
 }
@@ -48,7 +48,7 @@ void c_vdp_write_cram_cpu (u8 pal_num, void* data_ptr)
     	);
 }
 
-void c_vdp_issue_dma (u16 length, u16 dest, void* source)
+void c_vdp_issue_dma (u16 size_bytes, u16 dest, void* source)
 {
 	__asm__ volatile
 	(
@@ -57,7 +57,7 @@ void c_vdp_issue_dma (u16 length, u16 dest, void* source)
 		"move.l	%2, %%a0\n\t"
 		"bsr	vdp_issue_dma"
 		:
-		: "r" (length), "r" (dest), "m" (source)
+		: "r" (size_bytes), "r" (dest), "m" (source)
 		: "d0", "d1", "a0"
 	);
 }
